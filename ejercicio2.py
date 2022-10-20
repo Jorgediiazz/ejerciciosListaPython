0a97a90109a0b7db91619ce8ce027a5ecd1797ef
Jorgediiazz <113972610+Jorgediiazz@users.noreply.github.com>
2022-10-20 09:54:57 +0200

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -4,19 +4,20 @@ Crea una lista e inicializala con 5 cadenas de caracteres leídas por teclado.
 Copia los elementos de la lista en otra lista pero en orden inverso, y muestra sus elementos por la pantalla.
 '''
 
-def lista_caracteres(n):
+def lista_caracteres():
     lst=[]
-    for i in range(0,n):
-        cadena=int(input("Dime una palabra: ")
-        lst.append(palabra)
+    for i in range(0,5):
+        cadena=str(input("Dime una palabra: "))
+        lst.append(cadena)
     return lst
-print(lst)
-                   
-def pedirCadenasCaracteres():
-    pedirCadenasCaracteres(5)
-                   
-def ejercicio2():
-    lista=pedirCadenaCaracteres()
-    print(lst.reverse())
-    for i in lista:
-        print(i)
+lista=lista_caracteres()
+lista2=lista.copy()
+lista2.reverse()
+print(lista)
+print(lista2)
+
+
+'''
+def lista_inversa(lst):
+    lista=lst.reverse()
+print(lista)'''
