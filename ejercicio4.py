814b12c671be50bc28214c27fe93e93c4ddb01a6
Jorgediiazz <113972610+Jorgediiazz@users.noreply.github.com>
2022-10-24 12:09:51 +0200

    Update ejercicio4.py

--- ejercicio4.py
+++ ejercicio4.py
@@ -3,3 +3,17 @@
 Programa que declare una lista y la vaya llenando de núemros hasta que introducimos un número negativo.
 Entonces se debe imprmir el vector (sólo los elementos introducidos)
 '''
+
+#Función
+def lista():
+    lst=[]
+    try:
+        while(True):
+            n=int(input("Dime un numero: "))
+            if(n > 0):
+                lst.append(n)
+            else:
+                return lst
+    except:
+        print("Tiene que ser un numero")
+print(lista())
