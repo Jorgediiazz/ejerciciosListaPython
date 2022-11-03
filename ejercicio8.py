94bfe7d615405d93680d43b20d97db47126696a3
Jorgediiazz <113972610+Jorgediiazz@users.noreply.github.com>
2022-11-03 09:31:40 +0100

    Update ejercicio8.py

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,4 +1,3 @@
-
 # Ejercicio 8 #
 '''
 Denifinir una estructura de datos con listas que permita guardar la temperatura mínima y máxima de 5 días. 
@@ -34,13 +33,30 @@ def diasMenosTemperatura(lista):
     for i in lista:
         if(i[0]<=tempmin):
             tempmin=i[0]
-    print(tempmin)
+    print("Esta es la temperatura minima: ", tempmin)
     
     for i in range(0,len(lista)):
         if(tempmin==lista[i][0]):
             lista_minimos.append(i+1)
     return lista_minimos
 
+#Funcion que te pide un temperatura y muestre los dias cuya temperatura maxima coincida
+def buscarTemperaturaMaxima(lista1):
+    temp = float(input("Dime la temperatura:"))
+    
+    lista_dias_max = []
+    lista_max = []
+    
+    for i in lista1:
+        #Se agregan todas las temperaturas máximas a una lista vacia
+        lista_max.append(i[1])
+    
+    for i in range(0, len(lista_max)):
+        if(lista_max[i] == temp):
+            lista_dias_max.append(i+1)
+            return lista_dias_max
+    else:
+        print("No existe ningun día con esa temperatura")
 
 lista=listaTemperatura(5)
 lista2=listaTemperatura(10)
@@ -48,3 +64,5 @@ lista3=listaTemperatura(0)
 
 print("Esta es la lista de las temperaturas: ", lista)
 print("Esta es la lista de la temperatura media: ", temperaturaMedia(lista))
+print("Este es el dia/s con menos temperatura: ",diasMenosTemperatura(lista))
+print(buscarTemperaturaMaxima(lista))
